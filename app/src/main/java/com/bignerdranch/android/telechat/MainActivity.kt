56aa674c44444011bd9e263741e69b41b49286e9
dima <agodimon@mail.ru>
2021-10-26 22:45:30 +0500

    Добавил зависимости. Библиотеки: Mike Penz, Material Drawer: https://github.com/mikepenz/MaterialDrawer/tree/v7.0.0 добавил папки ui и model. -----1-----

--- app/src/main/java/com/bignerdranch/android/telechat/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/telechat/MainActivity.kt
@@ -2,8 +2,12 @@ package com.bignerdranch.android.telechat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewbinding.ViewBinding
+import com.bignerdranch.android.telechat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
