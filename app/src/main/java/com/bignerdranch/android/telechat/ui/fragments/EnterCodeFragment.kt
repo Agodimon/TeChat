859d9f5b4350adc6d2a45cf4f9fd681e547cb212
dima <agodimon@mail.ru>
2021-11-07 12:33:03 +0500

    добавил AppTextWatcher.kt и вынес туда лишний код, который получился из-за имплементации. создал funs.kt  и в нем реализовал функцию Toast

--- app/src/main/java/com/bignerdranch/android/telechat/ui/fragments/EnterCodeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/telechat/ui/fragments/EnterCodeFragment.kt
@@ -6,31 +6,24 @@ import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import com.bignerdranch.android.telechat.R
+import com.bignerdranch.android.telechat.utilits.AppTextWatcher
+import com.bignerdranch.android.telechat.utilits.showToast
 import kotlinx.android.synthetic.main.fragment_enter_code.*
 
 
 class EnterCodeFragment : Fragment(R.layout.fragment_enter_code) {
     override fun onStart() {
         super.onStart()
-        register_input_code.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        register_input_code.addTextChangedListener(AppTextWatcher {
                 val string = register_input_code.text.toString()
                 if (string.length == 6) {
                     verificateCode()
-                }
             }
 
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-
-            }
         })
     }
 
     fun verificateCode() {
-        Toast.makeText(activity, "Ok", Toast.LENGTH_SHORT).show()
+        showToast("OK")
     }
 }
---
+++ app/src/main/java/com/bignerdranch/android/telechat/utilits/AppTextWatcher.kt
@@ -0,0 +1,22 @@
+package com.bignerdranch.android.telechat.utilits
+
+import android.text.Editable
+import android.text.TextWatcher
+
+class AppTextWatcher(val onSuccess: (Editable?) -> Unit) : TextWatcher {
+
+
+    override fun afterTextChanged(s: Editable?) {
+        onSuccess(s)
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+    }
+
+
+}
